0f374a0509b92699f62832a8d401cd70c7731c9a
Nathan Burgess <nathan@3playmedia.com>
2018-01-15 09:15:21 -0500

    Revert back to api config, check hostname

--- src/api-config.js
+++ src/api-config.js
@@ -1,5 +1,6 @@
 let backendHost;
 const hostname = window && window.location && window.location.hostname;
+console.log(hostname);
 
 if(hostname === "https://whos-init.herokuapp.com/") {
   backendHost = "https://afternoon-sands-93107.herokuapp.com";
