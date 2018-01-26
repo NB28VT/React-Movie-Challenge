e4c0d71edba795f1491eefdebd409bbc588fbc90
Nathan Burgess <nathan@3playmedia.com>
2018-01-26 12:53:47 -0500

    Build up Cast Member child components

--- src/components/CastMember.js
+++ src/components/CastMember.js
@@ -1,39 +1,78 @@
 import React, { Component } from 'react';
-
+import * as apiConfigs from '../api-config.js';
+// TODO: maybe a placeholder for a profile pic?
+// TODO: if no image is available, don't display
+import filmCountdown from '../../public/images/film_countdown.gif'
 
 class CastMember extends React.Component {
-  handleCastSelection(castMember) {
-    if (castMember.id === this.props.castMember.id) {
-      this.props.registerPick(this.props.castMember.id, true);
-    } else {
-      this.props.registerPick(this.props.castMember.id, false);
+  constructor(props) {
+    super(props)
+    this.state = ({
+      imageSource: filmCountdown,
+      imageLoaded: false,
+      correct: null
+    })
+  }
+
+
+  componentWillMount(){
+    this.loadProfilePicture();
+  }
+
+  loadProfilePicture(){
+    const profilePicture = new Image()
+    profilePicture.src = `${apiConfigs.API_ROOT}/movie_thumbnail?poster_path=` + this.props.posterPath.substr(1).replace(/\.jpg/, "");
+    profilePicture.onload = () => {
+      this.setState({
+        imageSource: profilePicture.src,
+        imageLoaded: true
+      })
     }
   }
 
+  // handleCastSelection(castMember) {
+  //   if (castMember.id === this.props.castMember.id) {
+  //     this.props.registerPick(this.props.castMember.id, true);
+  //   } else {
+  //     this.props.registerPick(this.props.castMember.id, false);
+  //   }
+  // }
+  //
   calculateDivClass( ){
-    if (this.props.castMember.correct === true) {
-        return "castMember correctAnswer";
-    } else if (this.props.castMember.correct === false) {
-        return "castMember wrongAnswer";
-    }  else {
-      return "castMember";
+    if (this.state.correct === true) {
+        return "correctAnswer";
+    } else if (this.state.correct === false) {
+        return "wrongAnswer";
     }
   }
 
+  // render cast list as separate component?
+
+  // Try separate components:
   render() {
-    const thumbnailUrl = "Beh";
-    // const thumbnailUrl = `${API_ROOT}/cast_thumbnail?profile_picture_path=` +  this.props.castMember.profile_path.substr(1).replace(/\.jpg/, "");
     return(
-      <div className={this.calculateDivClass()}>
-        <img src={thumbnailUrl} alt={this.props.castMember.name} className="img-thumbnail castMemberThumbnail"/>
-        <div className= "btn-group-vertical castButtons" role="group">
-          {this.props.scrambledSelections.map((scrambledCastMember) => (
-             <button type="button" className="btn btn-default" onClick={this.handleCastSelection.bind(this, scrambledCastMember)}>{scrambledCastMember.name}</button>
-          ))}
-        </div>
+      <div className="castMember">
+        <CastThumbnail thumbnailUrl={this.state.imageSource} className={this.calculateImageClass()}/>
+        <CastSelections selections={this.props.scrambledSelections} registerPick={this.registerPick}/>
       </div>
     )
   }
+
+
+  // render() {
+  //   const thumbnailUrl = "Beh";
+  //   // const thumbnailUrl = `${API_ROOT}/cast_thumbnail?profile_picture_path=` +  this.props.castMember.profile_path.substr(1).replace(/\.jpg/, "");
+  //   return(
+  //     <div className={this.calculateDivClass()}>
+  //       <img src={thumbnailUrl} alt={this.props.castMember.name} className="img-thumbnail castMemberThumbnail"/>
+  //       <div className= "btn-group-vertical castButtons" role="group">
+  //         {this.props.scrambledSelections.map((scrambledCastMember) => (
+  //            <button type="button" className="btn btn-default" onClick={this.handleCastSelection.bind(this, scrambledCastMember)}>{scrambledCastMember.name}</button>
+  //         ))}
+  //       </div>
+  //     </div>
+  //   )
+  // }
 }
 
 export default CastMember
--- src/components/CastPicker.js
+++ src/components/CastPicker.js
@@ -6,11 +6,10 @@ class CastPicker extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      castLoaded: false
-      castSelections: []
+      castLoaded: false,
+      scrambledSelections: []
     }
   }
-
   // vs component did mount?
   componentWillMount() {
     this.loadMovieCast();
@@ -22,7 +21,7 @@ class CastPicker extends React.Component {
         .then((responseJson) => {
           const cast = responseJson.cast;
           if (cast.length > 0) {
-            this.setScrambledCast(cast.slice(0,5));
+            this.scrambleCastSelections(cast.slice(0,5));
           } else {
             // TODO: ADD ERROR HANDLING
             console.log("Error getting cast");
@@ -30,39 +29,41 @@ class CastPicker extends React.Component {
         })
   }
 
-  setScrambledCast(castMembers){
-    
-
-
-
-
-
+  scrambleCastSelections(castMembers){
+    const castArray = castMembers.slice();
+    // Fisher-Yates shuffle
+    var remainingCount = castArray.length, top, index;
+    while(remainingCount) {
+      // Pick a random remaining element
+      index = Math.floor(Math.random() * remainingCount--);
+      // Swap it with the current element
+      top = castArray[remainingCount];
+      castArray[remainingCount] = castArray[index];
+      castArray[index] = top;
+    }
+    // TODO: RETURN ONLY VALUES WE CARE ABOUT (ID, NAME)
+    this.setState({
+      scrambledSelections: castArray,
+      castLoaded: true
+    });
   }
 
 
-
   render(){
+    // TODO: NICER LOOKING LOADING MESSAGE
     if (this.state.castLoaded) {
       return(<h1>Loading...</h1>  )
     } else {
       return(
         <div className="row castRow">
-          {this.props.castData.map((castMember) => (
+          {this.state.scrambledSelections.map((castMember) => (
             <div>
-              <CastMember castMember={castMember} scrambledSelections={this.props.scrambledCast} registerPick={this.props.registerPick} />
+              <CastMember castMember={castMember} scrambledSelections={this.state.scrambledSelections} registerPick={this.props.registerPick} />
             </div>
           ))}
         </div>
       )
-
-
-
-
     }
-
-
-
-
   }
 }
 
