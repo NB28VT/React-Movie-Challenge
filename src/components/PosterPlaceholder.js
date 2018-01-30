9ef921f38c186a1b61bf2d26f6a155871553f077
Nathan Burgess <nathan@3playmedia.com>
2018-01-30 08:21:20 -0500

    Add poster placeholder component for movies with no poster image

---
+++ src/components/PosterPlaceholder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class PosterPlaceholder extends React.Component {
+
+  parseReleaseDate() {
+    const d = new Date(this.props.release_date);
+    return d.getFullYear();
+  }
+
+  render() {
+    return (
+      <div className="img-thumbnail movieChoice posterPlaceholder">
+        <h1>{this.props.name} ({this.parseReleaseDate()})</h1>
+      </div>
+    )
+  }
+}
+
+export default PosterPlaceholder
