f70b878b8d95191426f8262dddd505cb261c69e2
Nathan Burgess <nathan@3playmedia.com>
2018-02-11 09:01:57 -0500

    Re-style movie picker for responsive divs. Center 1 or 2 movie results

--- src/components/MoviePicker.js
+++ src/components/MoviePicker.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import MovieChoice from './MovieChoice'
+import '../styles/movie_picker.css';
 
 
 class MoviePicker extends Component {
   render() {
     return(
-      <div className="moviePicker">
-        <h1>Select Film:</h1>
-        {this.props.movieChoices.map((movie) => (
-          <MovieChoice key={movie.id} posterPath={movie.poster_path} movieID={movie.id} title={movie.title} release_date={movie.release_date} selectMovie={this.props.selectMovie}/>
-        ))}
+      <div className="container-fluid movie-picker">
+        <div className="row movie-picker-top"></div>
+        <div className="row movie-picker-middle">
+          <div className="col-md-2"></div>
+          <div className="col-md-8 poster-row">
+            {this.props.movieChoices.map((movie) => (
+              <MovieChoice posterClass={"img-thumbnail movie-poster"} key={movie.id} posterPath={movie.poster_path} movieID={movie.id} title={movie.title} release_date={movie.release_date} selectMovie={this.props.selectMovie}/>
+            ))}
+          </div>
+          <div className="col-md-2"></div>
+        </div>
+        <div className="row movie-picker-bottom"></div>
       </div>
+
     )
   }
 }
